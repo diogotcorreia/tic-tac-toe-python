37d091d98db052d6eb3a40578bd5b92f7104b814
Diogo Correia <me@diogotc.com>
2020-11-07 13:32:47 +0000

    Add eh_posicao_livre e obter_posicoes_livres
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -88,3 +88,48 @@ def tabuleiro_str(tab):
         if row != 2:
             result += '\n-----------\n'
     return result
+
+
+def pos_humana_maquina(pos):
+    """
+    Converte uma posicao humana (e.g. 1, 4, 9) numa posicao
+    maquina (e.g. (0,0), (1, 0), (2, 2))
+    """
+    if not eh_posicao(pos):
+        raise ValueError('pos_humana_maquina: o argumento e invalido')
+
+    pos -= 1
+    return (pos // 3, pos % 3)
+
+
+def pos_maquina_humana(row, col):
+    """
+    Converte uma posicao maquina (e.g. (0,0), (1, 0), (2, 2))
+    numa posicao humana (e.g. 1, 4, 9)
+    """
+    # TODO improve checking
+    if not eh_coluna_ou_linha(row + 1) or not eh_coluna_ou_linha(col + 1):
+        raise ValueError('pos_maquina_humana: algum dos argumentos e invalido')
+
+    return row * 3 + col + 1
+
+
+def eh_posicao_livre(tab, pos):
+    if not eh_tabuleiro(tab) or not eh_posicao(pos):
+        raise ValueError('eh_posicao_livre: algum dos argumentos e invalido')
+
+    posMaquina = pos_humana_maquina(pos)
+    return tab[posMaquina[0]][posMaquina[1]] == 0
+
+
+def obter_posicoes_livres(tab):
+    if not eh_tabuleiro(tab):
+        raise ValueError('obter_posicoes_livres: o argumento e invalido')
+
+    # TODO: consider using eh_posicao_livre
+    result = ()
+    for row in range(3):
+        for col in range(3):
+            if tab[row][col] == 0:
+                result += (pos_maquina_humana(row, col), )
+    return result
